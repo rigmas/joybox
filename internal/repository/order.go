1b70685850bcd63df3023ecee4ab2407c66d8c9b
SamuelGirsang <samueelgrs@gmail.com>
2023-01-26 22:28:48 +0700

    feat(orders): Implement repository for orders

---
+++ internal/repository/order.go
@@ -0,0 +1,25 @@
+package repository
+
+import "github.com/rigmas/joybox/internal/core/domain"
+
+type OrderRepo struct {
+	Orders []*domain.Order
+}
+
+func NewOrderRepo(orders []*domain.Order) (*OrderRepo, error) {
+	return &OrderRepo{
+		Orders: orders,
+	}, nil
+}
+
+func (o *OrderRepo) Add(order domain.Order) (domain.Order, error) {
+	o.Orders = append(o.Orders, &order)
+
+	return order, nil
+}
+
+func (o *OrderRepo) Get() ([]domain.Order, error) {
+	// o.Orders = append(o.Orders, &order)
+
+	return nil, nil
+}
