038934839daca71a6894a232b2aa0c8c2afc13f7
SamuelGirsang <samueelgrs@gmail.com>
2023-01-26 22:33:19 +0700

    feat(orders): Implement dummy data generator for order list

---
+++ pkg/generator/order.go
@@ -0,0 +1,38 @@
+package generator
+
+import (
+	"github.com/rigmas/joybox/internal/core/domain"
+	"github.com/rigmas/joybox/internal/utils"
+)
+
+func Order() []*domain.Order {
+	var mockOrders []*domain.Order
+	mockBook1 := domain.Book{
+		Key:           "/works/OL21177W",
+		Title:         "Wuthering Heights",
+		Author:        "Emily Brontë",
+		EditionNumber: "OL38586477M",
+	}
+	timeSchedule1, _ := utils.ScheduleConverter("2023-01-26")
+	mockOrder1 := domain.Order{
+		Book:           mockBook1,
+		PickupSchedule: timeSchedule1,
+	}
+
+	mockBook2 := domain.Book{
+		Key:           "/works/OL362427W",
+		Title:         "Romeo and Juliet",
+		Author:        "William Shakespeare",
+		EditionNumber: "OL26501367M",
+	}
+	timeSchedule2, _ := utils.ScheduleConverter("2023-01-30")
+	mockOrder2 := domain.Order{
+		Book:           mockBook2,
+		PickupSchedule: timeSchedule2,
+	}
+
+	mockOrders = append(mockOrders, &mockOrder1, &mockOrder2)
+
+	return mockOrders
+
+}
